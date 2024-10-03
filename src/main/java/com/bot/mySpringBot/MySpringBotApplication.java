9c73019d3e8a69c0068b605f078ac8371f6426de
Const <shimanoffsky@gmail.com>
2024-10-03 14:50:22 +0300

    first

---
+++ src/main/java/com/bot/mySpringBot/MySpringBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.mySpringBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/mySpringBot/MySpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.mySpringBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
